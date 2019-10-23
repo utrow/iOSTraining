9582aa14d212fb59b1242b908932fd67ed652c43
Yutaro Ohbuchi <buchitaro@live.jp>
2019-10-23 19:45:32 +0900

    Create Project 1-3

---
+++ Swift/practice/day1/1.3/SampleViewController/SampleViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleViewController
+//
+//  Created by 大渕 祐太郎 on 2019/10/23.
+//  Copyright © 2019 大渕 祐太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
