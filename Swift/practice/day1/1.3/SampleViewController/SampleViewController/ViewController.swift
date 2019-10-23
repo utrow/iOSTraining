51b209d136ee8901c16a297e04425de9836350a2
Yutaro Ohbuchi <buchitaro@live.jp>
2019-10-23 19:46:23 +0900

    Add practice storyboard label, button

--- Swift/practice/day1/1.3/SampleViewController/SampleViewController/ViewController.swift
+++ Swift/practice/day1/1.3/SampleViewController/SampleViewController/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        label.text = "Test"
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        label.text = "YES"
     }
 
 
